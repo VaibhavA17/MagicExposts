3da72fc3f0954be6addf831c7a9ca56b5fdbb76c
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-09-29 00:28:57 +0530

    adjustments

---
+++ js/pulgins.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('load', function(){
+    $(function(){
+        $("#nav-placeholder").load("../pulgins/nav.html");
+    });
+    
+    $(function(){
+        $("#footer-placeholder").load("../pulgins/footer.html");
+    });
+})
+
+
