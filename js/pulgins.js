4a982495ca5a3ef9981c9a6c64aa05f98b28aaa2
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-09-29 17:52:59 +0530

    slider

--- js/pulgins.js
+++ js/pulgins.js
@@ -13,7 +13,68 @@ window.addEventListener('load', function(){
     });
     
     $(function(){
-        $("#footer-placeholder").load("pulgins2/footer.html");
+        $("#footer-placeholdermain").load("pulgins2/footer.html");
     });
 })
+
+const scrollMenu = document.querySelector('.scrollmenu')
+const scrollBtns = document.querySelectorAll('#smbtn')
+const singleSlider = document.querySelector('.single-slider')
+
+scrollBtns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        const value = e.currentTarget.dataset.id
+        singleSlider.innerHTML = `<div><img class="img-responsive center-block" src="../img/products/pouf/ME-P-${value}.png" alt="slider"></div>`
+        count = value
+        carousel()
+    })
+
+})
+
+
+
+const slideBtns = document.querySelectorAll('.npbtn')
+const prevBtn = document.querySelector('.prev-slide')
+const nextBtn = document.querySelector('.next-slide')
+let count = 1
+
+slideBtns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList
+        if ( styles.contains("next-slide")){
+            count++
+            if (count<10){
+                singleSlider.innerHTML = `<div><img class="img-responsive center-block" src="../img/products/pouf/ME-P-00${count}.png" alt="slider"></div>`
+            }else{
+                singleSlider.innerHTML = `<div><img class="img-responsive center-block" src="../img/products/pouf/ME-P-0${count}.png" alt="slider"></div>`
+            }
+        }
+        else if ( styles.contains("prev-slide")){
+            count--
+            if (count<10){
+                singleSlider.innerHTML = `<div><img class="img-responsive center-block" src="../img/products/pouf/ME-P-00${count}.png" alt="slider"></div>`
+            }else{
+                singleSlider.innerHTML = `<div><img class="img-responsive center-block" src="../img/products/pouf/ME-P-0${count}.png" alt="slider"></div>`
+            }
+        }
+        carousel()
+        console.log(count)
+    })
+})
+
+
+function carousel() {
+    if (count < 35) {
+        nextBtn.style.display = "block";
+    } else {
+        nextBtn.style.display = "none";
+    }
+    if (count > 1) {
+        prevBtn.style.display = "block";
+    } else {
+        prevBtn.style.display = "none";
+    }
+}
+prevBtn.style.display = "none";
+
 
