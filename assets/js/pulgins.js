10e1e7513cc55cd0c928af01196c175d77c59317
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-10-02 14:00:10 +0530

    adjustments

---
+++ assets/js/pulgins.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function () {
+    $(function () {
+        $("#nav-placeholder").load("../assets/plugins/nav.html");
+    });
+
+    $(function () {
+        $("#footer-placeholder").load("../assets/plugins/footer.html");
+    });
+
+    $(function () {
+        $("#nav-placeholdermain").load("assets/plugins/nav.html");
+    });
+
+    $(function () {
+        $("#footer-placeholdermain").load("assets/plugins/footer.html");
+    });
+})
