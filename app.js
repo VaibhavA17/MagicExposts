6f1f9e686a0e92a93e3f05531fa0c4292339b8de
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2021-10-07 00:13:28 +0530

    app

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ db.connect((error) => {
 app.use('/', require('./routes/pages'))
 app.use('/auth', require('./routes/auth'))
 
-// app.listen(5011, () => {
-//     console.log("sever started on 5005")
-// })
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
